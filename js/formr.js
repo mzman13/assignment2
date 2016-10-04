371fe02a18ec903f6572ecb711d6ae42962f6da4
miz37 <miz37@pitt.edu>
2016-10-04 01:43:54 -0400

    added basic form with javascript validation and css style

---
+++ js/formr.js
@@ -0,0 +1,6 @@
+var customerNode = document.getElementById("myName");
+var phoneNode = document.getElementById("phone");
+var emailNode = document.getElementById("email");
+customerNode.addEventListener("change", chkName, false);
+phoneNode.addEventListener("change", chkPhone, false);
+emailNode.addEventListener("change", chkEmail, false);
