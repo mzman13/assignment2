c6ab433218c7b0f62bbc147ecf19b3b21db1fc62
miz37 <miz37@pitt.edu>
2016-10-04 01:48:36 -0400

    remove formr.js

--- js/formr.js
+++
@@ -1,6 +0,0 @@
-var customerNode = document.getElementById("myName");
-var phoneNode = document.getElementById("phone");
-var emailNode = document.getElementById("email");
-customerNode.addEventListener("change", chkName, false);
-phoneNode.addEventListener("change", chkPhone, false);
-emailNode.addEventListener("change", chkEmail, false);
