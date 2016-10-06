2693a3e3cdd03f55ac4dcf57cb8f32ab09beaab4
miz37 <miz37@pitt.edu>
2016-10-06 18:00:33 -0400

    changed onsubmit to false

--- js/form.js
+++ js/form.js
@@ -7,23 +7,20 @@ function checkInputs() {
   
   if (!myName || myName == null) {
     alert("Error: Please enter a name\n");
-    return false;
   }
   if (myPhone.search(/^\d{3}-\d{3}-\d{4}$/) != 0 || !myPhone || myPhone == null) {
     alert("Invalid phone number (" +myPhone+
-    "). \nThe correct form is: (ddd-ddd-dddd) \n");
-    return false;
+    "). \nThe correct form is: (ddd-ddd-dddd) \n"); 
   }
 
   //@ is first character, last 3 characters include '.' (.co), '.' is next to @
   if (at < 1 || dot+2 >= myEmail.length || dot < at+2 || !myEmail || myEmail == null) {
     alert("Invalid Email Address. (" +myEmail+ ")\nFormat is name@url");
-    return false;
   } else {
     document.getElementById("header").style.visibility = "hidden";
     document.getElementById("form").style.visibility = "hidden";
     document.getElementById("footer").style.visibility = "hidden";
     alert("Thanks for submitting!");
-    return true;
   }
+  return false;
 }
